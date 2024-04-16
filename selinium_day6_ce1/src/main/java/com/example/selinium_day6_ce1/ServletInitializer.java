7e845063f8d0499cb79e765172897efb664f0552
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2024-04-16 15:01:51 +0530

    Add files via upload

---
+++ selinium_day6_ce1/src/main/java/com/example/selinium_day6_ce1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.selinium_day6_ce1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SeliniumDay6Ce1Application.class);
+	}
+
+}
---
+++ selinium_day6_ce1/src/test/java/com/example/selinium_day6_ce1/SeliniumDay6Ce1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selinium_day6_ce1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeliniumDay6Ce1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
