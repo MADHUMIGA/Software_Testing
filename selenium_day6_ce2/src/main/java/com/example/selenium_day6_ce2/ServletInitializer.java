14cd8122926f6248991a7e5889853cf97dcfb8c5
Madhumiga_Vijayakumar <124855723+MADHUMIGA@users.noreply.github.com>
2024-04-23 09:24:08 +0530

    Add files via upload

---
+++ selenium_day6_ce2/src/main/java/com/example/selenium_day6_ce2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.selenium_day6_ce2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SeleniumDay6Ce2Application.class);
+	}
+
+}
